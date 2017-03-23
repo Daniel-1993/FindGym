e75c12be5e900bbf8aef037e74f359d012be8ef0
Daniel Marcos <daniel.1993marcos@gmail.com>
2017-03-22 22:02:13 -0300

    Criando tela de cadastro de Usuario

--- app/src/main/java/com/example/daniel/findgym/PrincipalActivity.java
+++ app/src/main/java/com/example/daniel/findgym/PrincipalActivity.java
@@ -1,7 +1,10 @@
 package com.example.daniel.findgym;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        Button btnCadastroUsuario = (Button) findViewById(R.id.btnusuario);
+
+        btnCadastroUsuario.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PrincipalActivity.this, CadastroUsuarioActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
