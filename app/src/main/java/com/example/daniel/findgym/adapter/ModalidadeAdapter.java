397eed00ffb20e62974888ce81b7cb16d8f9d7e0
Daniel Marcos <daniel.1993marcos@gmail.com>
2017-03-26 11:09:00 -0300

    Criando tela de ListView de Modalidade

--- app/src/main/java/com/example/daniel/findgym/adapter/ModalidadeAdapter.java
+++ app/src/main/java/com/example/daniel/findgym/adapter/ModalidadeAdapter.java
@@ -1,9 +1,39 @@
 package com.example.daniel.findgym.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.daniel.findgym.R;
+import com.example.daniel.findgym.model.Modalidade;
+
+import java.util.ArrayList;
 
 /**
  * Created by daniel on 25/03/17.
  */
-public class ModalidadeAdapter  {
+public class ModalidadeAdapter extends ArrayAdapter<Modalidade> {
+    private final Context context;
+    private final ArrayList<Modalidade> listamodalidades;
+
+    public ModalidadeAdapter(Context context, ArrayList<Modalidade> listamodalidades) {
+        super(context, R.layout.linhamodalidade);
+        this.context = context;
+        this.listamodalidades = listamodalidades;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.linhamodalidade, parent, false);
+        TextView descricao = (TextView) rowView.findViewById(R.id.pDescricao);
+        TextView treinador = (TextView) rowView.findViewById(R.id.pTreinador);
+        descricao.setText(listamodalidades.get(position).getDescricao());
+        treinador.setText(listamodalidades.get(position).getTreinador().getNomeTreinador());
+        return rowView;
+    }
 }
